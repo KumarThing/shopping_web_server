7f069bab628b7649e6a0ffcfa5fcd9d7612d71c0
KumarThing <102548843+KumarThing@users.noreply.github.com>
2025-12-09 18:13:21 +0200

    updated main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
 )
 
 var (
@@ -28,16 +29,28 @@ func main() {
 		if r.Method == http.MethodPost{
 			r.ParseForm()
 
+			delIndex := r.FormValue("delete")
+			if delIndex != ""{
+				i ,_ := strconv.Atoi(delIndex)
+				if i >=0 && i < len(list){
+					list = append(list[:i], list[i+1:]...)
+				}
+			}
+
 			newList := r.FormValue("item")
 			
 			if newList != "" {
 				list = append(list, newList)
 			}
 
-			addTmpl.Execute(w, list)
-			return
+			
 		}
-			addTmpl.Execute(w, list)
+		data := struct {
+			List []string
+		}{
+			List:list,
+		}
+			addTmpl.Execute(w, data)
 	})
 
 	http.HandleFunc("/showall", func(w http.ResponseWriter, r *http.Request){
@@ -48,6 +61,7 @@ func main() {
 		
 	})
 
+	
 
 	fmt.Println("Your server is running in http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
